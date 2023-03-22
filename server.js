79373d0612cd83352f180d2425e50b72a58657c3
Brooksteven <Brooksteven580@gmail.com>
2023-03-22 14:26:04 -0400

    added back gitignore and fixed server.js by switching around the api and html routes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //Dependencies
 const express = require('express');
-const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
 
 //checks Express App
 const app = express();
